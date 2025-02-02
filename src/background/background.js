1ee05e1441ce11655e442a82feb9528249c80b7a
hercjay <iamhercjay@gmail.com>
2025-02-02 23:59:56 +0000

    migrate chrome runtime message listeners to the service worker background js file and import relevant functions from messagehandler using ES6 syntax

--- src/background/background.js
+++ src/background/background.js
@@ -1,3 +1,10 @@
+
+
+import { getRandomMessages, createCategory, addMessageToCategory } from './messageHandler.js';
+
+
+
+
 chrome.runtime.onInstalled.addListener(() => {
     console.log("AdFriend Extension Installed");
   
@@ -17,4 +24,42 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.onChanged.addListener((changes) => {
     console.log("Settings Updated:", changes);
   });
+
+
+
+
+
+
+
+// Listen for messages and respond
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log("AdFriend: Message received from background js file", request);
+  switch (request.action) {
+    case 'getRandomMessages':
+      console.log("AdFriend: getRandomMessages request received " + request.numOfMsgs);
+      getRandomMessages(request.numOfMsgs).then((messages) => {
+        sendResponse({ messages });
+      }).catch((error) => {
+        sendResponse({ error });
+      });
+      return true; // Will respond asynchronously
+    case 'createCategory':
+      createCategory(request.categoryKey, request.categoryTitle).then((value) => {
+        sendResponse({ value });
+      }).catch((error) => {
+        sendResponse({ error });
+      });
+      return true; // Will respond asynchronously
+    case 'addMessageToCategory':
+      addMessageToCategory(request.categoryKey, request.message).then((value) => {
+        sendResponse({ value });
+      }).catch((error) => {
+        sendResponse({ error });
+      });
+      return true; // Will respond asynchronously
+    default:
+      sendResponse({ error: 'Unknown action' });
+      return false;
+  }
+});
   
