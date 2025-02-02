f2fd5461d682fe2812cf132864dbc153bd7ae120
hercjay <iamhercjay@gmail.com>
2025-02-02 21:00:22 +0000

    setup getPluginEnabledValue and listenForPluginEnabledChanges function in main content js file to control the replacement of ads based on stored value and force a reload on changes to reflect the new value

--- src/content-scripts/content.js
+++ src/content-scripts/content.js
@@ -4,8 +4,6 @@ console.log("AdFriend Content Script Loaded");
 
 function replaceAds() {
 
-function replaceAds() {
-
     // Get the patterns from global adSelector variable defined in manifest.json and adSelector.js
   const adElements = document.querySelectorAll(window.adSelector);
 
@@ -41,48 +39,36 @@ function getRandomMessage() {
   return messages[Math.floor(Math.random() * messages.length)];
 }
 
-// Run the function when the page loads
-replaceAds();
 
-const adElements = document.querySelectorAll(adSelector);
-
-  console.log("AdFriend: number of ads on this page is " + adElements.length);
-
-  adElements.forEach((ad) => {
-    let widget = document.createElement("div");
-    widget.className = "AdFriend-widget";
-    widget.innerHTML = `
-      <div class="AdFriend-message">
-        <h3>🌟 Stay Inspired!</h3>
-        <p>${getRandomMessage()}</p>
-      </div>
-    `;
-    //TODO: I will migrate the styles to a separate file using the classnames
-    widget.style.cssText = `
-      background: black;
-      padding: 15px;
-      border-radius: 5px;
-      text-align: center;
-      font-size: 14px;
-      color: white;
-    `;
-
-    console.log("AdFriend: Replacing this ad " + ad.getHTML() + " with this widget " + widget.getHTML());
-
-    ad.replaceWith(widget);
+function getPluginEnabledValue() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get('enabled', (data) => {
+      if (data.enabled !== undefined) {
+        resolve(data.enabled);
+      } else {
+        resolve(true); // default to true if no value is found
+      }
+    });
   });
 }
 
-//TODO: I will reconstruct this to categorize the messages and maybe use a statements API
-function getRandomMessage() {
-  const messages = [
-    "Keep pushing forward!",
-    "You are capable of amazing things!",
-    "Take a deep breath and keep going!",
-    "Your hard work will pay off!"
-  ];
-  return messages[Math.floor(Math.random() * messages.length)];
+function listenForPluginEnabledChanges() {
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+        if (changes.enabled) {
+        console.log("AdFriend: Plugin enabled status has changed", changes.enabled.newValue);
+            //reload page to show/hide ads based on new value
+            location.reload();
+        }
+    });
 }
 
-// Run the function when the page loads
-replaceAds();
+// Run the function when the page loads, if this plugin is enabled in the popup
+getPluginEnabledValue().then((enabled) => {
+    console.log("AdFriend: Plugin enabled status is", enabled);
+  if (enabled === true) {
+    replaceAds();
+  } 
+});
+
+listenForPluginEnabledChanges();
+
