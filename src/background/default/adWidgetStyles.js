92f08b9d8b7384d59916784a61776da6459251ae
hercjay <iamhercjay@gmail.com>
2025-02-07 19:23:42 +0000

    update the default adwidget styles to use flex and hidden for showlogo slot. add more default styles

--- src/background/default/adWidgetStyles.js
+++ src/background/default/adWidgetStyles.js
@@ -3,32 +3,31 @@
 
 export const defaultAdWidgetStyles = {
     dark: {
-      styleTitle: 'Dark Style (Default)',
-      background: 'black',
-      padding: '15px',
-      borderRadius: '5px',
-      textAlign: 'center',
-      fontSize: '14px',
-      color: 'rgba(255, 255, 255, 0.8)',
-      titleTextColor: 'white',
-      logoSize: '22px',
-      showLogo: 'visible'
+        styleTitle: 'Dark Style (Default)',
+        background: 'black',
+        padding: '15px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        fontSize: '14px',
+        color: 'rgba(255, 255, 255, 0.8)',
+        titleTextColor: 'white',
+        logoSize: '22px',
+        showLogo: 'flex'
     },
     light: {
-      styleTitle: 'Light Style (Default)',
-      background: 'white',
-      padding: '15px',
-      borderRadius: '5px',
-      textAlign: 'center',
-      fontSize: '14px',
-      color: 'rgba(5, 5, 5, 0.8)',
-      titleTextColor: 'black',
-      logoSize: '22px',
-      showLogo: 'visible'
+        styleTitle: 'Light Style (Default)',
+        background: 'white',
+        padding: '15px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        fontSize: '14px',
+        color: 'rgba(5, 5, 5, 0.8)',
+        titleTextColor: 'black',
+        logoSize: '22px',
+        showLogo: 'flex'
     },
-    //teal: rgb(0, 128, 128)
-    teal: {
-        styleTitle: 'Teal Style I (Default)',
+    tealDefault: {
+        styleTitle: 'Teal Style (Default)',
         background: 'rgb(0, 36, 36)',
         padding: '15px',
         borderRadius: '5px',
@@ -37,6 +36,91 @@ export const defaultAdWidgetStyles = {
         color: 'rgba(255, 255, 255, 0.8)',
         titleTextColor: 'rgb(0, 255, 255)',
         logoSize: '22px',
-        showLogo: 'visible'
-      }
-  };
+        showLogo: 'flex'
+    },
+    blueDefault: {
+        styleTitle: 'Blue Style (Default)',
+        background: 'rgb(0, 0, 128)',
+        padding: '15px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        fontSize: '14px',
+        color: 'rgba(255, 255, 255, 0.8)',
+        titleTextColor: 'rgb(173, 216, 230)',
+        logoSize: '22px',
+        showLogo: 'flex'
+    },
+    greenDefault: {
+        styleTitle: 'Green Style (Default)',
+        background: 'rgb(0, 128, 0)',
+        padding: '15px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        fontSize: '14px',
+        color: 'rgba(255, 255, 255, 0.8)',
+        titleTextColor: 'rgb(144, 238, 144)',
+        logoSize: '22px',
+        showLogo: 'flex'
+    },
+    purpleDefault: {
+        styleTitle: 'Purple Style (Default)',
+        background: 'rgb(75, 0, 130)',
+        padding: '15px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        fontSize: '14px',
+        color: 'rgba(255, 255, 255, 0.8)',
+        titleTextColor: 'rgb(216, 191, 216)',
+        logoSize: '22px',
+        showLogo: 'flex'
+    }
+    ,
+    redDefault: {
+        styleTitle: 'Red Style (Default)',
+        background: 'rgb(128, 0, 0)',
+        padding: '15px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        fontSize: '14px',
+        color: 'rgba(255, 255, 255, 0.8)',
+        titleTextColor: 'rgb(255, 99, 71)',
+        logoSize: '22px',
+        showLogo: 'flex'
+    },
+    yellowDefault: {
+        styleTitle: 'Yellow Style (Default)',
+        background: 'rgb(255, 255, 0)',
+        padding: '15px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        fontSize: '14px',
+        color: 'rgba(0, 0, 0, 0.8)',
+        titleTextColor: 'rgb(0, 0, 0)',
+        logoSize: '22px',
+        showLogo: 'flex'
+    },
+    pinkDefault: {
+        styleTitle: 'Pink Style (Default)',
+        background: 'rgb(255, 192, 203)',
+        padding: '15px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        fontSize: '14px',
+        color: 'rgba(0, 0, 0, 0.8)',
+        titleTextColor: 'rgb(255, 20, 147)',
+        logoSize: '22px',
+        showLogo: 'flex'
+    },
+    brownDefault: {
+        styleTitle: 'Brown Style (Default)',
+        background: 'rgb(139, 69, 19)',
+        padding: '15px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        fontSize: '14px',
+        color: 'rgba(255, 255, 255, 0.8)',
+        titleTextColor: 'rgb(210, 180, 140)',
+        logoSize: '22px',
+        showLogo: 'flex'
+    },
+};
