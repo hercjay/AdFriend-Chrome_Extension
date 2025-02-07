e2b519ce92566cd36d2349040259e5f8a9cf8a24
hercjay <iamhercjay@gmail.com>
2025-02-07 10:30:42 +0000

    setup a file for ad widget default styles

---
+++ src/background/default/adWidgetStyles.js
@@ -0,0 +1,29 @@
+
+
+
+export const defaultAdWidgetStyles = {
+    light: {
+      styleTitle: 'Light Style (Default)',
+      background: 'black',
+      padding: '15px',
+      borderRadius: '5px',
+      textAlign: 'center',
+      fontSize: '14px',
+      color: 'rgba(255, 255, 255, 0.8)',
+      titleTextColor: 'white',
+      logoSize: '22px',
+      showLogo: 'visible'
+    },
+    dark: {
+      styleTitle: 'Dark Style (Default)',
+      background: 'white',
+      padding: '15px',
+      borderRadius: '5px',
+      textAlign: 'center',
+      fontSize: '14px',
+      color: 'rgba(5, 5, 5, 0.8)',
+      titleTextColor: 'black',
+      logoSize: '22px',
+      showLogo: 'visible'
+    }
+  };
