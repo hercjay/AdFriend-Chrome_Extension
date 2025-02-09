c5da130b3a3984a430d559224c0d5e509d4be5b6
hercjay <iamhercjay@gmail.com>
2025-02-09 14:38:30 +0000

    create a file to hold the default ad widget contents

---
+++ src/background/default/adWidgetContents.js
@@ -0,0 +1,47 @@
+export const defaultAdWidgetContents = {
+    motivationalQuotesDefault: {
+      enabled: true,
+      title: "Stay Motivated!",
+      type: "Motivational Quotes",
+      messages: [
+        "Believe in yourself and all that you are!",
+        "The only limit to our realization of tomorrow is our doubts of today.",
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        "Your potential is endless. Go do what you were created to do.",
+        "Dream big and dare to fail.",
+        "The harder you work for something, the greater you'll feel when you achieve it.",
+        "Don't watch the clock; do what it does. Keep going.",
+        "The future belongs to those who believe in the beauty of their dreams."
+      ]
+    },
+    activityRemindersDefault: {
+      enabled: true,
+      title: "Stay Active!",
+      type: "Activity Reminders",
+      messages: [
+        "Take a short walk to refresh your mind.",
+        "Remember to stretch and move around.",
+        "Stay hydrated, drink a glass of water.",
+        "Take a few deep breaths and relax.",
+        "Stand up and do a quick stretch.",
+        "Take a moment to look away from your screen and rest your eyes.",
+        "Do a quick set of jumping jacks to get your blood flowing.",
+        "Take a few minutes to meditate and clear your mind."
+      ]
+    },
+    funFactsDefault: {
+        enabled: false,
+        title: "Did You Know?",
+        type: "Fun Facts",
+        messages: [
+          "Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still edible.",
+          "A day on Venus is longer than a year on Venus. It takes Venus longer to rotate once on its axis than to complete one orbit of the Sun.",
+          "Bananas are berries, but strawberries aren't. Botanically, a berry is a fruit produced from the ovary of a single flower with seeds embedded in the flesh.",
+          "Octopuses have three hearts. Two pump blood to the gills, while the third pumps it to the rest of the body.",
+          "There are more stars in the universe than grains of sand on all the Earth's beaches.",
+          "A bolt of lightning contains enough energy to toast 100,000 slices of bread.",
+          "The Eiffel Tower can be 15 cm taller during the summer due to the expansion of iron in the heat.",
+          "Wombat poop is cube-shaped. This helps it stay in place and mark their territory."
+        ]
+      }
+  };
--- src/background/messageHandler.js
+++ src/background/messageHandler.js
@@ -1,43 +1,14 @@
-const defaultCategories = {
-  motivationalQuotes: {
-    enabled: true,
-    title: "Stay Motivated!",
-    messages: [
-      "Believe in yourself and all that you are!",
-      "The only limit to our realization of tomorrow is our doubts of today.",
-      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-      "Your potential is endless. Go do what you were created to do.",
-      "Dream big and dare to fail.",
-      "The harder you work for something, the greater you'll feel when you achieve it.",
-      "Don't watch the clock; do what it does. Keep going.",
-      "The future belongs to those who believe in the beauty of their dreams."
-    ]
-  },
-  activityReminders: {
-    enabled: true,
-    title: "Stay Active!",
-    messages: [
-      "Take a short walk to refresh your mind.",
-      "Remember to stretch and move around.",
-      "Stay hydrated, drink a glass of water.",
-      "Take a few deep breaths and relax.",
-      "Stand up and do a quick stretch.",
-      "Take a moment to look away from your screen and rest your eyes.",
-      "Do a quick set of jumping jacks to get your blood flowing.",
-      "Take a few minutes to meditate and clear your mind."
-    ]
-  }
-};
+
+
   
   // Load categories from storage or use default
   export function  loadCategories() {
-    return new Promise((resolve) => {
-      chrome.storage.sync.get('msgCategories', (data) => {
-        if (data.msgCategories) {
-          resolve(data.msgCategories);
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.get('adWidgetContents', (data) => {
+        if (data.adWidgetContents) {
+          resolve(data.adWidgetContents);
         } else {
-          saveCategories(defaultCategories);
-          resolve(defaultCategories);
+          reject('AdFriend: No message categories found in storage.');
         }
       });
     });
@@ -46,7 +17,7 @@ const defaultCategories = {
   // Save categories to storage
   export function  saveCategories(categories) {
     return new Promise((resolve) => {
-      chrome.storage.sync.set({ msgCategories: categories }, resolve);
+      chrome.storage.sync.set({ adWidgetContents: categories }, resolve);
     });
   }
 
@@ -96,26 +67,42 @@ const defaultCategories = {
 
 
 // Get random messages
- export function  getRandomMessages (numOfMsgs = 1) {
-    return loadCategories().then((categories) => {
-      const categoryKeys = Object.keys(categories);
-      //filter for only enabled categories and categories with messages
-      const enabledCategoryKeys = categoryKeys.filter((key) => (categories[key].enabled && categories[key].messages.length > 0));
-      const messages = [];
-  
-      for (let i = 0; i < numOfMsgs; i++) {
-        const randomCategoryKey = enabledCategoryKeys[Math.floor(Math.random() * enabledCategoryKeys.length)];
-        const randomCategory = categories[randomCategoryKey];
-        const randomMessage = randomCategory.messages[Math.floor(Math.random() * randomCategory.messages.length)];
-        messages.push({
-          title: randomCategory.title,
-          content: randomMessage
-        });
+export function getRandomMessages(numOfMsgs = 1) {
+
+  console.log("AdFriend: getRandomMessages request received " + numOfMsgs);
+  return loadCategories().then((categories) => {
+    let validCatKeys = [];
+    for(const categoryKey in categories){
+      // filter for enabled categories
+      if(categories[categoryKey].enabled == true && categories[categoryKey].messages.length > 0){
+        validCatKeys.push(categoryKey);
       }
-  
-      return messages;
-    });
-  };
+    }
+
+    if (validCatKeys.length < 1) {
+      console.log("AdFriend: No enabled categories with messages found.");
+      //return placeholder messages
+      return Array.from({ length: numOfMsgs }, (_, i) => ({
+        title: "AdFriend",
+        content: "No messages found. Please enable a category with messages in the 'Widget Content' section of the AdFriend plugin popup page."
+      }));
+    }
+
+    const messages = [];
+
+    for (let i = 0; i < numOfMsgs; i++) {
+      const randomCategoryKey = validCatKeys[Math.floor(Math.random() * validCatKeys.length)];
+      const randomCategory = categories[randomCategoryKey];
+      const randomMessage = randomCategory.messages[Math.floor(Math.random() * randomCategory.messages.length)];
+      messages.push({
+        title: randomCategory.title,
+        content: randomMessage
+      });
+    }
+
+    return messages;
+  });
+}
   
 
 
