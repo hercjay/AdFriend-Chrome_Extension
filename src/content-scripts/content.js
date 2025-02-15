7dd363a8dacd6918694c8e0361e22e073c252852
hercjay <iamhercjay@gmail.com>
2025-02-15 16:44:33 +0000

    start working on bypassing some anti-adblockers

--- src/content-scripts/content.js
+++ src/content-scripts/content.js
@@ -9,9 +9,49 @@ const myQuizList = {
   //I can add ore here
 }
 
+
+
+// Check for the presence of the debugger statement
+try {
+  debugger;
+} catch (e) {
+  if (e instanceof Error && e.message.includes('debugger')) {
+    console.log('AdFriend: Debugger detected, taking evasive action');
+    // You can add your evasive action code here
+    // Override the debugger statement
+    const originalDebugger = window.debugger;
+    window.debugger = function() {
+      // Do nothing or execute a custom function
+      console.log("Debugger override: Ignoring pause request");
+    };
+  }
+}
+
+// Detect the use of eval or Function constructors
+if (typeof eval === 'function' || typeof Function === 'function') {
+  // eval or Function constructor is present, take evasive action
+  console.log('AdFriend: eval or Function constructor detected, taking evasive action');
+  // You can add your evasive action code here
+  // Override the debugger statement
+  const originalDebugger = window.debugger;
+  window.debugger = function() {
+    // Do nothing or execute a custom function
+    console.log("AdFriend: Debugger override: Ignoring pause request");
+  };
+}
+
+// Monitor for changes in the script's execution context
+if (typeof window !== 'undefined' && window !== this) {
+  // Execution context has changed, take evasive action
+  console.log('AdFriend: Execution context changed, taking evasive action');
+  // You can add your evasive action code here
+}
+
+
 function replaceAds() {
     // Get the patterns from global adSelector variable defined in manifest.json and adSelector.js
-    const adElements = document.querySelectorAll(window.AdFriend_adSelector);
+    const adSelector = window.AdFriend_adSelector;
+    const adElements = document.querySelectorAll(adSelector);
   
     console.log("AdFriend: number of ads on this page is " + adElements.length);
   
@@ -95,6 +135,8 @@ function replaceAds() {
                 });
               });
             } else if(message.shouldCardFlip == true) {
+              //replace newline chars with line breaks
+              const processedContent = message.content.replace(/\n/g, '<br>');
               widget.innerHTML = `
                   <div class="AdFriend-message">
 
@@ -115,7 +157,7 @@ function replaceAds() {
                         </div>
 
                         <div class="card-back msg-p">
-                            ${message.content}
+                            <strong>${processedContent}</strong>
                         </div>
 
                       </div>
@@ -124,7 +166,7 @@ function replaceAds() {
                   </div>
                 `;
                 let attempted = false;
-                widget.querySelector('.flip-card').addEventListener('click', () => {
+                widget.querySelector('.card-inner').addEventListener('click', () => {
                   widget.querySelector('.flip-card').classList.toggle('flipped');
                   
                   if(attempted == false) {
